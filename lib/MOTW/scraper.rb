187c425c83e2498455d05745c1cb8026011e144a
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-06-14 00:00:28 -0400

    Add duration imdb scrape and rename element to node

--- lib/MOTW/scraper.rb
+++ lib/MOTW/scraper.rb
@@ -2,24 +2,29 @@ class Scraper
   # attr_accessor
 
   def initialize
+=begin
     imdb = Nokogiri::HTML(open("https://www.imdb.com/movies-in-theaters"))
 
-    imdb.css('div.list_item').each { |element|
-      title = element.css('a')[1].text.strip
-      description = element.css('div.outline').text.strip
+    imdb.css('div.list_item').each { |node|
+      title = node.css('a')[1].text.strip if node.css('a')[1]
+      description = node.css('div.outline').text.strip if node.css('div.outline')
       # TODO use collect then convert to string
-      genre = element.css('p.cert-runtime-genre span')[0].text
-      metascore = element.css('div.rating_txt span').text.strip
-      # TODO use collect and ignore 0
-      stars = element.css('div.txt-block a')[1].text.strip
-      rating = element.css('img')[1].attribute('title').value if element.css('img')[1]
-      director = element.css('div.txt-block a')[0].text.strip
+      genre = node.css('p.cert-runtime-genre span')[0].text.strip if node.css('p.cert-runtime-genre span')[0]
+      metascore = node.css('div.rating_txt span').text.strip if node.css('div.rating_txt span')
+      # TODO use collect then convert to string and ignore 0
+      stars = node.css('div.txt-block a')[1].text.strip if node.css('div.txt-block a')[1]
+      rating = node.css('img')[1].attribute('title').value if node.css('img')[1]
+      director = node.css('div.txt-block a')[0].text.strip if node.css('div.txt-block a')[0]
+      duration = node.css('time').text.strip if node.css('time')
+      binding.pry
     }
+=end
 
+#=begin
+    rt = Nokogiri::HTML(open("https://rottentomatoes.com/m/incredibles_2"))
+    tester_2 = rt.css('div.movie_synopsis').text.strip
+#=end
 
   end
 
 end
-
-#rt = Nokogiri::HTML(open("https://rottentomatoes.com/m/incredibles_2"))
-#tester_2 = rt.css('div.movie_synopsis').text.strip
