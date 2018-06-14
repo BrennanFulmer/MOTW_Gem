44548c4529160d936286b4d41d0a6088e295ab32
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-06-13 23:44:19 -0400

    Determine imdb scrapes

--- lib/MOTW.rb
+++ lib/MOTW.rb
@@ -1,6 +1,9 @@
-require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
 require_relative './MOTW/version'
 require_relative './MOTW/scraper'
+
+
+
+require 'open-uri'
--- lib/MOTW/scraper.rb
+++ lib/MOTW/scraper.rb
@@ -1,8 +1,25 @@
 class Scraper
-  attr_accessor :doc
+  # attr_accessor
 
   def initialize
-    self.doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/browse/opening/"))
+    imdb = Nokogiri::HTML(open("https://www.imdb.com/movies-in-theaters"))
+
+    imdb.css('div.list_item').each { |element|
+      title = element.css('a')[1].text.strip
+      description = element.css('div.outline').text.strip
+      # TODO use collect then convert to string
+      genre = element.css('p.cert-runtime-genre span')[0].text
+      metascore = element.css('div.rating_txt span').text.strip
+      # TODO use collect and ignore 0
+      stars = element.css('div.txt-block a')[1].text.strip
+      rating = element.css('img')[1].attribute('title').value if element.css('img')[1]
+      director = element.css('div.txt-block a')[0].text.strip
+    }
+
+
   end
 
 end
+
+#rt = Nokogiri::HTML(open("https://rottentomatoes.com/m/incredibles_2"))
+#tester_2 = rt.css('div.movie_synopsis').text.strip
