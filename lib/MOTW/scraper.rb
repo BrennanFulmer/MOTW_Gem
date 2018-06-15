58a7c869806e4d0d23ee229c3103f464a7b50089
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-06-15 01:43:22 -0400

    Add film_types method to Scraper
    working on turning rotten tomatoes scrapes into a hash, needed this new method to assign genre value

--- lib/MOTW/scraper.rb
+++ lib/MOTW/scraper.rb
@@ -1,18 +1,18 @@
 class Scraper
 
-  def self.jonra(element)
-    if element.css('p.cert-runtime-genre span')[0]
-      element.css('p.cert-runtime-genre span').collect { |genre|
-        genre.text.strip
-      }.join.gsub('|', ' | ')
-    end
-  end
-
-  def self.kast(element)
+  def self.list_kast(element)
     if element.css('div.txt-block a')[1]
       element.css('div.txt-block a').collect { |star|
         star.text.strip
       }.drop(1).join(' | ')
+    end
+  end
+
+  def self.list_jonra(element)
+    if element.css('p.cert-runtime-genre span')[0]
+      element.css('p.cert-runtime-genre span').collect { |genre|
+        genre.text.strip
+      }.join.gsub('|', ' | ')
     end
   end
 
@@ -22,11 +22,11 @@ class Scraper
 
     imdb.css('div.list_item').each { |node|
       movie = {
-        cast: kast(node),
+        cast: list_kast(node),
         description: if a = node.css('div.outline') then a.text.strip end,
         director: if b = node.css('div.txt-block a')[0] then b.text.strip end,
         duration: if c = node.css('time') then c.text.strip end,
-        genre: jonra(node),
+        genre: list_jonra(node),
         metascore: if d = node.css('div.rating_txt span') then d.text.strip end,
         rating: if e = node.css('img')[1] then e.attribute('title').value.strip end,
         title: if f = node.css('a')[1] then f.text.strip.split(' (')[0] end
@@ -34,14 +34,29 @@ class Scraper
       movie[:rating] = '' unless movie[:rating]
       movie_list.push(movie)
     }
-    
+
     movie_list
   end
 
-  def self.scrape_movie(search_term)
-    search = 'incredibles_2'
-    rt = Nokogiri::HTML(open('https://rottentomatoes.com/m/' + "#{search}"))
+  def self.film_types(page)
+    if page.css('li.meta-row')[1].css('a')[0]
+      page.css('li.meta-row')[1].css('a').collect { |type|
+        type.text.strip
+      }.join(' | ')
+    end
+  end
 
+  def self.scrape_movie # (search_term)
+    search = 'incredibles_2'
+    rt = Nokogiri::HTML(open("https://rottentomatoes.com/m/#{search}"))
+
+    film = {
+      description: if g = rt.css('div.movie_synopsis') then g.text.strip end,
+      genre: film_types(rt),
+      title: if h = rt.css('h1.title')[0] then h.text.strip end
+    }
+
+=begin
     title = rt.css('h1.title')[0].text.strip if rt.css('h1.title')[0]
     description = rt.css('div.movie_synopsis').text.strip if rt.css('div.movie_synopsis')
       # TODO use collect then convert to string
@@ -49,7 +64,7 @@ class Scraper
     critic_tomatometer = rt.css('div.critic-score')[0].text.strip if rt.css('div.critic-score')[0]
     user_tomatometer = rt.css('div.audience-score span.superPageFontColor').text.strip if rt.css('div.audience-score span.superPageFontColor')
       # TODO use collect then convert to string
-      stars = rt.css('div.cast-item span')[0].text.strip if rt.css('div.cast-item span')[0]
+      cast = rt.css('div.cast-item span')[0].text.strip if rt.css('div.cast-item span')[0]
       role = rt.css('div.cast-item span')[1].text.strip if rt.css('div.cast-item span')[1]
     rating = rt.css('li.meta-row div.meta-value')[0].text.strip if rt.css('li.meta-row div.meta-value')[0]
     director = rt.css('li.meta-row div.meta-value')[2].text.strip if rt.css('li.meta-row div.meta-value')[2]
@@ -60,6 +75,8 @@ class Scraper
       studio = rt.css('li.meta-row div.meta-value')[6].text.strip if rt.css('li.meta-row div.meta-value')[6]
     consensus = rt.css('p.critic_consensus')[0].text.strip
     year = rt.css('span.year')[0].text.strip if rt.css('span.year')[0]
+=end
+binding.pry
   end
 
 end
