69c1db5cb8f298c346e033a7c44d555ce3905f5c
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-06-14 22:01:00 -0400

    Add Rotten Tomatoes scrapes

--- lib/MOTW/scraper.rb
+++ lib/MOTW/scraper.rb
@@ -8,23 +8,42 @@ class Scraper
     imdb.css('div.list_item').each { |node|
       title = node.css('a')[1].text.strip if node.css('a')[1]
       description = node.css('div.outline').text.strip if node.css('div.outline')
-      # TODO use collect then convert to string
-      genre = node.css('p.cert-runtime-genre span')[0].text.strip if node.css('p.cert-runtime-genre span')[0]
+        # TODO use collect then convert to string
+        genre = node.css('p.cert-runtime-genre span')[0].text.strip if node.css('p.cert-runtime-genre span')[0]
       metascore = node.css('div.rating_txt span').text.strip if node.css('div.rating_txt span')
-      # TODO use collect then convert to string and ignore 0
-      stars = node.css('div.txt-block a')[1].text.strip if node.css('div.txt-block a')[1]
+        # TODO use collect then convert to string and ignore 0
+        stars = node.css('div.txt-block a')[1].text.strip if node.css('div.txt-block a')[1]
       rating = node.css('img')[1].attribute('title').value if node.css('img')[1]
       director = node.css('div.txt-block a')[0].text.strip if node.css('div.txt-block a')[0]
       duration = node.css('time').text.strip if node.css('time')
-      binding.pry
     }
 =end
 
 #=begin
-    rt = Nokogiri::HTML(open("https://rottentomatoes.com/m/incredibles_2"))
-    tester_2 = rt.css('div.movie_synopsis').text.strip
+    search = 'incredibles_2'
+    rt = Nokogiri::HTML(open('https://rottentomatoes.com/m/' + "#{search}"))
+
+    title = rt.css('h1.title')[0].text.strip if rt.css('h1.title')[0]
+    description = rt.css('div.movie_synopsis').text.strip if rt.css('div.movie_synopsis')
+      # TODO use collect then convert to string
+      genre = rt.css('div.meta-value a')[0].text.strip if rt.css('div.meta-value a')[0]
+    critic_tomatometer = rt.css('div.critic-score')[0].text.strip if rt.css('div.critic-score')[0]
+    user_tomatometer = rt.css('div.audience-score span.superPageFontColor').text.strip if rt.css('div.audience-score span.superPageFontColor')
+      # TODO use collect then convert to string
+      cast = rt.css('div.cast-item span')[0].text.strip if rt.css('div.cast-item span')[0]
+      role = rt.css('div.cast-item span')[1].text.strip if rt.css('div.cast-item span')[1]
+    rating = rt.css('li.meta-row div.meta-value')[0].text.strip if rt.css('li.meta-row div.meta-value')[0]
+    director = rt.css('li.meta-row div.meta-value')[2].text.strip if rt.css('li.meta-row div.meta-value')[2]
+      # if the movie is available on DVD its rt.css('div.meta-value time')[2].text.strip
+      duration = rt.css('div.meta-value time')[1].text.strip if rt.css('div.meta-value time')[1]
+    writer = rt.css('li.meta-row div.meta-value')[3].text.strip if rt.css('li.meta-row div.meta-value')[3]
+      # if the movie is available on DVD its rt.css('li.meta-row div.meta-value')[7].text.strip
+      studio = rt.css('li.meta-row div.meta-value')[6].text.strip if rt.css('li.meta-row div.meta-value')[6]
+    consensus = rt.css('p.critic_consensus')[0].text.strip
+    year = rt.css('span.year')[0].text if rt.css('span.year')[0]
 #=end
 
+      binding.pry
   end
 
 end
