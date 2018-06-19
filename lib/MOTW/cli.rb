4959d37a4a7f11092042f19f408850719caed11a
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-06-18 20:57:21 -0400

    Add #movie_list to Cli class
    Also added several pseudo coded methods to cli for movie list functionality.

--- lib/MOTW/cli.rb
+++ lib/MOTW/cli.rb
@@ -3,21 +3,47 @@ class Cli
   # attr_accessor
 
   def initialize
+    # greets, and calls methods
+  end
+
+  def menu
+    # present options to the user (list, lookup, exit)
+  end
+
+  def valid_menu_input
+    # validate user input
+  end
+
+  def call_menu_option
+    # go to users selected option from the menu
+  end
+
+  def movie_list
     Movie.create_from_collection(Scraper.scrape_list)
     display_list
+    list_choices
   end
 
   def display_list
+    puts ''
     Movie.all.each_with_index { |v, i|
-      puts "
-        #{v.title}"
-      unless v.metascore == ''
-        puts "  #{v.metascore} Metascore"
-      end
-      puts "#{v.description}
-      "
+      puts "#{i + 1}  #{v.title}"
+      puts "#{v.description}"
+      puts "  #{v.metascore} Metascore" unless v.metascore == ''
+      puts ''
     }
   end
 
+  def list_choices
+    # options are more info on movie in the list, or exit
+  end
+
+  def valid_list_choice
+    # validation for list_choices
+  end
+
+  def call_list_option
+    # go to selected list choice
+  end
 
 end
